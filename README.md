456d130cd808e8e13340973f6575401c6ecfc267
Mustafa Bal <5262061+mstfbl@users.noreply.github.com>
2021-05-14 15:01:55 -0700

    Added Az DevOps PR Build Trigger

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
----
-page_type: sample
-description: "A minimal sample app that can be used to demonstrate deploying Flask apps to Azure App Service on Linux."
-languages:
-- python
-products:
-- azure
-- azure-app-service
----
+# PyTorch PR Listener
 
-# Python Flask sample for Azure App Service (Linux)
+This is a Flask app deployed at https://pytorch-pr-web-service.azurewebsites.net that can listen for PyTorch PR updates.
 
-This is a minimal Flask app that can be deployed to Azure App Service on Linux.
-
-For instructions on running and deploying the code, see [Quickstart: Create a Python app in Azure App Service on Linux](https://docs.microsoft.com/azure/app-service/quickstart-python).
-
-## Contributing
-
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+To run locally:
+```bash
+# Create and start virtual environment
+python3 -m venv venv
+source venv/bin/activate
+# Install dependencies
+pip install -r requirements.txt
+# Run Flaskapp locally
+FLASK_APP=app:app flask run
+```
--- app.py
+++ app.py
@@ -1,6 +1,53 @@
-from flask import Flask
+import os
+import sys
+import requests
+import json
+from flask import Flask, request, abort, jsonify
 app = Flask(__name__)
 
+# Azure DevOps WebHook trigger variables
+AZURE_DEVOPS_BASE_WEBHOOK_URL = "https://dev.azure.com/aiinfra/_apis/public/distributedtask/webhooks/"
+AZURE_DEVOPS_TRIGGER_NAME = "GitHubPyTorchPRTrigger"
+AZURE_DEVOPS_TRIGGER_ADD_ON = "?api-version=6.0-preview"
+TRIGGER_URL = AZURE_DEVOPS_BASE_WEBHOOK_URL + AZURE_DEVOPS_TRIGGER_NAME + AZURE_DEVOPS_TRIGGER_ADD_ON
+
+# List of GitHub PyTorch branches we are currently tracking for custom tests
+GITHUB_PYTORCH_TRACKED_BRANCHES = ("master")
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return "Running..."
+
+@app.route("/prwebhook", methods=['POST'])
+def github_webhook_endpoint():
+    # Parse GitHub WebHook data
+    github_webhook_data = request.get_json()
+    if github_webhook_data is None:
+        abort(400, "Received JSON is NoneType")
+    if "pull_request" not in github_webhook_data:
+        abort(400, "JSON does not contain PR details")
+    if "number" not in github_webhook_data["pull_request"]:
+        abort(400, "JSON does not contain PR number details")
+    if "base" not in github_webhook_data["pull_request"]:
+        abort(400, "JSON does not contain PR base details")
+    if "ref" not in github_webhook_data["pull_request"]["base"]:
+        abort(400, "JSON does not contain PR base ref details")
+    if "action" not in github_webhook_data:
+        abort(400, "JSON does not contain PR action data")
+
+    # Obtain PyTorch PR information
+    pr_base_ref = github_webhook_data["pull_request"]["base"]["ref"]
+    pr_number = github_webhook_data["pull_request"]["number"]
+    if github_webhook_data["action"] != "opened" and github_webhook_data["action"] != "synchronize":
+        return "PR WebHook update is not that of a new opened or updated PR. Exiting..."
+    if pr_base_ref not in GITHUB_PYTORCH_TRACKED_BRANCHES:
+        return "PR does not target a targeted PyTorch branch. Exiting..."
+
+    # Build and send HTTP POST Trigger
+    s = requests.Session()
+    s.headers.update({"Authorization": "None"})
+    run_build_raw = s.post(TRIGGER_URL, json={
+        "repositoryName": "pytorch_tests",
+        "TARGET_BRANCH_TO_CHECK_AZ_DEVOPS_PR": "refs/pull/{0}/head".format(pr_number)
+    })
+    return "Build submitted"
